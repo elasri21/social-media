7239bb978a44083ec1caa4277169c1a36fddebcb
elasri21 <islame.edu@gmail.com>
2022-03-17 12:03:31 +0100

    fixing some issues

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const light = document.querySelector(".light");
 const dark = document.querySelector(".dark");
 const stayleSheet = document.getElementById("style-sheet")
 light.addEventListener("click", function(){
-    stayleSheet.setAttribute("href", "light-theme.css")
+    stayleSheet.setAttribute("href", "main-light.css")
 })
 dark.addEventListener("click", function(){
-    stayleSheet.setAttribute("href", "style.css")
+    stayleSheet.setAttribute("href", "main-dark.css")
 })
